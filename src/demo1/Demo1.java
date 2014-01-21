366a895e6187214021ae110dd80528c9087880eb
User 1 <larsesspam@gmail.com>
2014-01-21 16:30:04 +0100

    Initial Commit

---
+++ src/demo1/Demo1.java
@@ -0,0 +1,19 @@
+/*
+ */
+
+package demo1;
+
+/**
+ *
+ * @author Lars Mortensen (lam@cphbusiness.dk)
+ */
+public class Demo1 {
+
+  /**
+   * @param args the command line arguments
+   */
+  public static void main(String[] args) {
+    // TODO code application logic here
+  }
+  
+}
